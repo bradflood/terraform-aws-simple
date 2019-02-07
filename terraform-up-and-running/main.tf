1825fd57e9abdaa46748d2abaf4d81ce9acd947f
Brad Flood <bflood@keyholesoftware.com>
2019-02-06 18:37:26 -0600

    more learning

--- terraform-up-and-running/main.tf
+++ terraform-up-and-running/main.tf
@@ -1,10 +1,22 @@
 provider "aws" { 
-	region = "us-east-2" 
+	region = "us-east-1" 
+}
+
+variable "server_port" {
+	description = "The port the server will use for HTTP requests"
+	default = 8080
 }
 
 resource "aws_instance" "bubcus" {
-	ami = "ami-04328208f4f0cf1fe"
+	ami = "ami-40d28157"
 	instance_type = "t2.micro"
+	vpc_security_group_ids = ["${aws_security_group.bubcus_sg.id}"] 
+	user_data = <<-EOF
+	#!/bin/bash
+	echo "Hello World from Terraform" >> index.html
+	nohup busybox httpd -f -p "${var.server_port}" &
+	EOF
+	
 	tags = {
     	Name = "bubcus"
     	Terraform = "true"
@@ -13,3 +25,16 @@ resource "aws_instance" "bubcus" {
   	}
 }
 
+resource "aws_security_group" "bubcus_sg" {
+	name = "terraform-bubcus-instance"
+	ingress {
+		from_port = "${var.server_port}"
+		to_port = "${var.server_port}"
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0" ] 
+	}
+}
+
+output "public_ip" {
+	value = "${aws_instance.bubcus.public_ip}"
+}
